8a8863ff6eb48f52682bc117ca01c12e41c115bf
Lucas Leblow <leblowl@gmail.com>
2014-12-12 14:44:33 -0800

    fix ref for new om dep and remove touch btn hacks for component

--- src/cljs/lokate/components.cljs
+++ src/cljs/lokate/components.cljs
@@ -3,46 +3,38 @@
             [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
             [goog.string :as gstring]
+            [clojure.string :as string]
             [lokate.util :refer [display]]))
 
 (defn input [data owner {:keys [id className edit-key on-edit on-key-down] :as opts}]
   (reify
-    om/IInitState
-    (init-state [_]
-      {:exit-type nil})
-
     om/IDidMount
     (did-mount [_]
-      (.focus (om/get-node owner edit-key)))
+      (.focus (om/get-node owner (str edit-key))))
 
-    om/IRenderState
-    (render-state [_ {:keys [exit-type]}]
+    om/IRender
+    (render [_]
       (dom/div #js {:id "input-wrapper"}
         (dom/div #js {:id id
-                      :ref edit-key
+                      :ref (str edit-key)
                       :className className
-                      :style (display (not exit-type))
                       :contentEditable "true"
                       :onKeyDown on-key-down
-                      :onBlur (fn []
-                                (om/set-state! owner :exit-type "out")
-                                (js/setTimeout
-                                  #(on-edit (.-innerHTML (om/get-node owner edit-key))) 100))
                       :dangerouslySetInnerHTML #js {:__html (edit-key data)}})
         (dom/div #js {:id "input-ok"
-                      :style (display (not (= exit-type "out")))
-                      :onClick (fn []
-                                 (om/set-state! owner :exit-type "btn")
-                                 (js/setTimeout
-                                   #(on-edit (.-innerHTML (om/get-node owner edit-key))) 100))}
+                      :onClick #(on-edit (.-innerHTML (om/get-node owner (str edit-key))))}
           (dom/span #js {:id "input-ok-mark"}
             (gstring/unescapeEntities "&#10003;")))))))
+
+(defn blankf [str]
+  (when (not (string/blank? str))
+    str))
 
 (defn select [selectable owner {:keys [name-default path-fn] :as opts}]
   (om/component
     (dom/li #js {:className "select-list-item"}
       (dom/a #js {:className "select"
-                  :onClick #(put! (om/get-shared owner :nav) (or (:path selectable)
+                  :onClick #(put! (om/get-shared owner :nav) (or (blankf (:path selectable))
                                                                (path-fn selectable)))}
         (dom/span #js {:className "select-title"} (or (:name selectable) name-default))))))
 
